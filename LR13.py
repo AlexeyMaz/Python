e37cce4965857278b32eb85ae4dbdc0fb1ef03e9
Al Maz <alexmaznitskiy@gmail.com>
2022-09-23 23:52:17 +0300

    LR13_2

--- LR13.py
+++ LR13.py
@@ -1,10 +1,26 @@
+class Animal():
+    def __init__(self, input_name):
+        self.__pr_name = input_name
+        print(f"Родилось животное {self.__pr_name} ")
+
+    def eat(self):
+        print("Намнём")
+    def makeNoise(self):
+        print(f"{self.__pr_name} говорит Гррр")
+
+    def setName(self, new_name):
+        self.__pr_name = new_name
+    def getName(self):
+        return self.__pr_name
+    name = property(getName, setName)
+
 class Cat():
     name, color, weight = "", "", 0
     def meow(self):
         print(f"{self.name} говорит 'мяу'")
 
-cat = Cat()
-cat.name = "egor"
-cat.color = "ginger"
-cat.weight = "100000"
-cat.meow()
+tiger = Animal("oleg")
+print(tiger.name)
+tiger.name = "akakiy"
+tiger.eat()
+tiger.makeNoise()
